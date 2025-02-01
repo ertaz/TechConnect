3b751a70f4d1ce489e3ba58a15b4c22d4b1608be
ertaz <ez70509@ubt-uni.net>
2025-02-02 00:03:58 +0100

    Edit.php

---
+++ Edit.php
@@ -0,0 +1,60 @@
+<?php
+$userId = $_GET['id'];
+
+include_once'userRepository.php';
+
+$userRepository = new UserRepository();
+
+$user  = $userRepository->getUserById($userId);
+
+if(isset($_POST['editBtn'])){
+    $id = $user['id'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $role = $_POST['role'];
+
+    $password = $_POST['password'];
+    if (!empty($password)) {
+        // Hash the new password
+        $password = password_hash($password, PASSWORD_DEFAULT);
+    } else {
+        // If no new password is provided, use the existing password
+        $password = $user['password'];
+    }
+
+    $userRepository->updateUser($id, $name, $surname, $email, $username, $password, $role);
+
+    header("Location: Dashboard.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit User</title>
+    <link rel="stylesheet" href="Edit-css.css?v=1.0">
+</head>
+<body>
+    <h3>Edit User</h3>
+    <form action="" method="post">
+        <input type="text" name="name" value="<?=$user['name']?>"> <br> <br>
+        
+        <input type="text" name="surname" value="<?=$user['surname']?>"> <br> <br>
+        
+        <input type="text" name="email" value="<?=$user['email']?>"> <br> <br>
+
+        <input type="text" name="username" value="<?=$user['username']?>"> <br> <br>
+        
+        <input type="password" name="password"  placeholder="Enter a new password"> <br> <br>
+
+        <input type="text" name="role" value="<?=$user['role']?>"> <br> <br>
+
+        <input type="submit" name="editBtn" value="Save Changes"> <br> <br>
+    </form>
+</body>
+</html>
