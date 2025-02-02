da785b0126299d841271a3d3b07ab64e22462fb2
LauraSherifi <ls70756@ubt-uni.net>
2025-02-02 23:08:20 +0100

    Profile php-final

--- Profile.php
+++ Profile.php
@@ -5,6 +5,17 @@ if (!isset($_SESSION['user_id'])) {
     header('Location: LogIn-form.php');
     exit();
 }
+
+include_once 'Database.php';
+include_once 'User.php';
+
+$db = new Database();
+$connection = $db->getConnection();
+$user = new User($connection);
+
+
+$userDetails = $user->getUserDataByEmail($_SESSION['email']);
+
 ?>
 
 <!DOCTYPE html>
@@ -24,8 +35,8 @@ if (!isset($_SESSION['user_id'])) {
         </div>
         <div class="button">
             <a href="Home.php"><button type="button">Home</button></a>
-            <a href="About us.php"><button type="button">About Us</button></a>
-            <button type="button">Jobs</button>
+            <a href="About_us.php"><button type="button">About Us</button></a>
+            <a href="Job-listing.php"><button type="button">Jobs</button></a>
             <a href="ContactUs.php"><button type="button">Contact Us</button></a>
             
             <?php
@@ -41,35 +52,35 @@ if (!isset($_SESSION['user_id'])) {
 
 <div class="container">
     <div class="profile-pic">
-        <img src="<?php echo isset($_SESSION['profile_picture']) ? htmlspecialchars($_SESSION['profile_picture']) : 'default-profile.jpg'; ?>" alt="Profile Picture" id="profile-picture">
+        <img src="<?php echo isset($userDetails['profile_picture']) && $userDetails['profile_picture'] ? htmlspecialchars($userDetails['profile_picture']) : 'default-profile.jpg'; ?>" alt="Profile Picture" id="profile-picture">
     </div>
 
     <div class="about-me">
         <h2>About Me</h2>
-        <li><?php echo htmlspecialchars($_SESSION['about_me']); ?></li>
+        <p><?php echo isset($userDetails['about_me']) ? htmlspecialchars($userDetails['about_me']) : 'No information available'; ?></p>
     </div>
 
     <div class="details">
         <h2>Details</h2>
         <ul>
-            <li><strong>Name:</strong> <?php echo htmlspecialchars($_SESSION['name']) . " " . htmlspecialchars($_SESSION['surname']); ?></li>
+            <li><strong>Name:</strong> <?php echo htmlspecialchars($userDetails['name']) . " " . htmlspecialchars($userDetails['surname']); ?></li>
             <li><strong>Email:</strong> <?php echo htmlspecialchars($_SESSION['email']); ?></li>
-            <li><strong>Age:</strong> <?php echo htmlspecialchars($_SESSION['age']); ?></li>
-            <li><strong>Location:</strong> <?php echo htmlspecialchars($_SESSION['location']); ?></li>
+            <li><strong>Age:</strong> <?php echo htmlspecialchars($userDetails['age']); ?></li>
+            <li><strong>Location:</strong> <?php echo htmlspecialchars($userDetails['location']); ?></li>
         </ul>
 
         <div class="social-media">
-            <?php if (isset($_SESSION['facebook']) && $_SESSION['facebook']) { ?>
-                <a href="<?php echo htmlspecialchars($_SESSION['facebook']); ?>"><img src="facebook1.png" alt="Facebook"></a>
+            <?php if (isset($userDetails['facebook']) && $userDetails['facebook']) { ?>
+                <a href="<?php echo htmlspecialchars($userDetails['facebook']); ?>"><img src="facebook1.png" alt="Facebook"></a>
             <?php } ?>
-            <?php if (isset($_SESSION['instagram']) && $_SESSION['instagram']) { ?>
-                <a href="<?php echo htmlspecialchars($_SESSION['instagram']); ?>"><img src="instagram1.png" alt="Instagram"></a>
+            <?php if (isset($userDetails['instagram']) && $userDetails['instagram']) { ?>
+                <a href="<?php echo htmlspecialchars($userDetails['instagram']); ?>"><img src="instagram1.png" alt="Instagram"></a>
             <?php } ?>
-            <?php if (isset($_SESSION['linkedin']) && $_SESSION['linkedin']) { ?>
-                <a href="<?php echo htmlspecialchars($_SESSION['linkedin']); ?>"><img src="linkedin1.png" alt="LinkedIn"></a>
+            <?php if (isset($userDetails['linkedin']) && $userDetails['linkedin']) { ?>
+                <a href="<?php echo htmlspecialchars($userDetails['linkedin']); ?>"><img src="linkedin1.png" alt="LinkedIn"></a>
             <?php } ?>
-            <?php if (isset($_SESSION['other_social']) && $_SESSION['other_social']) { ?>
-                <a href="<?php echo htmlspecialchars($_SESSION['other_social']); ?>"><img src="social-icon.png" alt="Other Social"></a>
+            <?php if (isset($userDetails['other_social']) && $userDetails['other_social']) { ?>
+                <a href="<?php echo htmlspecialchars($userDetails['other_social']); ?>"><img src="social-icon.png" alt="Other Social"></a>
             <?php } ?>
         </div>
     </div>
