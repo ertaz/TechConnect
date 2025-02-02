bc559520c5917e4cca6bb63ee7dd09bd26e091be
ertaz <ez70509@ubt-uni.net>
2025-02-02 23:21:06 +0100

    Edit.php - Final

--- Edit.php
+++ Edit.php
@@ -1,22 +1,21 @@
 <?php
 $userId = $_GET['id'];
 
-include_once'userRepository.php';
+include_once'userRepository';
 
 $userRepository = new UserRepository();
 
 $user  = $userRepository->getUserById($userId);
 
 if(isset($_POST['editBtn'])){
-    $id = $user['id'];
+    $id = $user['Id'];
     $name = $_POST['name'];
     $surname = $_POST['surname'];
     $email = $_POST['email'];
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $role = $_POST['role'];
 
-    $userRepository->updateUser($id, $name, $surname, $email, $username, $password, $role);
+    $userRepository->updateUser($id, $name, $surname, $email, $username, $password);
 
     header("Location: Dashboard.php");
 }
@@ -29,22 +28,21 @@ if(isset($_POST['editBtn'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit User</title>
-    <link rel="stylesheet" href="Edit-css.css?v=1.0">
 </head>
 <body>
     <h3>Edit User</h3>
     <form action="" method="post">
-        <input type="text" name="name" value="<?=$user['name']?>"> <br> <br>
+        <input type="text" name="id" value="<?=$user['Id']?>" readonly> <br> <br>
         
-        <input type="text" name="surname" value="<?=$user['surname']?>"> <br> <br>
+        <input type="text" name="name" value="<?=$user['Name']?>"> <br> <br>
         
-        <input type="text" name="email" value="<?=$user['email']?>"> <br> <br>
-
-        <input type="text" name="username" value="<?=$user['username']?>"> <br> <br>
+        <input type="text" name="surname" value="<?=$user['Surname']?>"> <br> <br>
         
-        <input type="password" name="password"  placeholder="Enter a new password"> <br> <br>
-
-        <input type="text" name="role" value="<?=$user['role']?>"> <br> <br>
+        <input type="text" name="email" value="<?=$user['Email']?>"> <br> <br>
+        
+        <input type="text" name="username" value="<?=$user['Username']?>"> <br> <br>
+        
+        <input type="text" name="password" value="<?=$user['Password']?>"> <br> <br>
 
         <input type="submit" name="editBtn" value="Save Changes"> <br> <br>
     </form>
